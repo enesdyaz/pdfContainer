5ce0f914ff16ff69efc9093a359d53c2717d89ec
haryong jung <haryongjung@192-168-1-196.tpgi.com.au>
2021-02-12 09:59:05 +0800

    first

---
+++ components/PdfContent.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="pdf-content">
+      asdf
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    }
+  },
+
+  mounted() {
+    this.$nextTick(() => {
+      setTimeout(() => {
+        this.$emit("domRendered");
+      }, 1000);
+    });
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.pdf-content {
+  width: 100%;
+  background: #fff;
+
+}
+</style>
---
+++ store/pdfStore.js
@@ -0,0 +1,15 @@
+export const state = () => ({
+    controlValue: {
+        showLayout: false,
+        floatLayout: true,
+        enableDownload: true,
+        previewModal: true,
+        paginateElementsByHeight: 1100,
+        manualPagination: false,
+        filename: 'Hee Hee',
+        pdfQuality: 2,
+        pdfFormat: 'a4',
+        pdfOrientation: 'portrait',
+        pdfContentWidth: '800px'
+    }
+})
